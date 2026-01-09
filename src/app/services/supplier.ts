1bd92f79c2704fd09dfb34af749ef008ef192707
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-01-09 20:00:34 -0300

    add method email in supplier service

--- src/app/services/supplier.ts
+++ src/app/services/supplier.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ProductRequest } from '../models/product/product-request';
 import { SupplierRequest } from '../models/supplier/supplier-request';
+import { SupplierOrder } from '../models/supplier/supplier-order';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -39,4 +41,13 @@ export class Product {
     return this.http.delete(url);
   }
 
+   searchSupplierByEmail(email : string){
+    const url = `${this.apiUrl}/${email}`;
+    return this.http.get<any>(url);
+  }
+
+  sendOrderToSupplier(items: SupplierOrder[], supplierId: number): Observable<void> {
+    const url = `${this.apiUrl}/${supplierId}/send-order`;
+    return this.http.post<void>(url, items);
+  }
 }
