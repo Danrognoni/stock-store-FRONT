c783fe8d3bcfaa8f134421ff8acb9f07f125af30
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-01-09 19:36:13 -0300

    product Service

--- src/app/services/category.ts
+++ src/app/services/category.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CategoryRequest } from '../models/category/category-request';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Category {
-  
+  readonly apiurl = "localhost:8080/api/categories"
+  http = inject(HttpClient)
+
+  getCategories(page : number, size:number){
+    let params = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.http.get<any>(this.apiurl, {params});
+  }
+
+  getCategory(id:string){
+    const url = `${this.apiurl}/id/${id}`;
+    return this.http.get<any>(url);
+  }
+
+  searchCategory(name : string){
+    const url = `${this.apiurl}/${name}`
+    return this.http.get<any>(url);
+  }
+
+  postCategory(data:CategoryRequest){
+    return this.http.post<any>(this.apiurl, data);
+  }
+
+  patchCategory(id:string, data : Partial<CategoryRequest>){
+    const url = `${this.apiurl}/${id}`
+    return this.http.patch<any>(url, data);
+  }
+
+  deleteCategory(id : string){
+    const url = `${this.apiurl}/${id}`;
+    return this.http.delete(url);
+  }
+
 }
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ProductRequest } from '../models/product/product-request';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Product {
-  
+  readonly apiUrl = "localhost:8080/api/products";
+  http = inject(HttpClient);
+
+  getProducts(page : number, size : number){
+    let params = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.http.get<any>(this.apiUrl, {params});
+  }
+
+  getProduct(id : string){
+    const url = `${this.apiUrl}/id/${id}`;
+    return this.http.get<any>(url);
+  }
+
+  searchProduct(name : string){
+    const url = `${this.apiUrl}/${name}`;
+    return this.http.get<any>(url);
+  }
+
+  postProduct(data : ProductRequest){
+    return this.http.post<any>(this.apiUrl, data);
+  }
+
+  patchProduct(id:string, data : Partial<ProductRequest>){
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.patch<any>(url, data);
+  }
+
+  deleteProduct(id : string){
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url);
+  }
 }
