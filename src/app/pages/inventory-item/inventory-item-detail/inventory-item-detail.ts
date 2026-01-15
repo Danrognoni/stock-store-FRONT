c519750032f7d1b357d58f33a6bd432517eb0a66
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-01-15 14:34:27 -0300

    Update inventory-item-detail.ts

--- src/app/pages/inventory-item/inventory-item-detail/inventory-item-detail.ts
+++ src/app/pages/inventory-item/inventory-item-detail/inventory-item-detail.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { inventoryItemService } from './../../../services/inventory-item';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { single } from 'rxjs';
+import { InventoryItemDet } from '../../../models/inventoryItem/inventory-item-det';
 
 @Component({
   selector: 'app-inventory-item-detail',
@@ -7,5 +11,33 @@ import { Component } from '@angular/core';
   styleUrl: './inventory-item-detail.css',
 })
 export class InventoryItemDetail {
+  private router = inject(Router);
+  public inventoryItemService = inject(inventoryItemService);
+  public inventoryitem = signal<InventoryItemDet|null>(null);
+  private ruta = inject(ActivatedRoute);
 
+  constructor(){}
+
+  ngOnInit(){
+
+  }
+
+  getInventoryItemById(){
+    const id = this.ruta.snapshot.paramMap.get('id');
+    if(id){
+      this.inventoryItemService.getInventoryItem(id).subscribe({
+        next : ( data) =>{
+          this.inventoryitem.set(data);
+        },
+        error :  (e) =>{
+          console.error(e);
+
+        }
+      })
+    }
+  }
+
+  getBack(){
+    this.router.navigate(['/inventoryItem/inventoryItemList'])
+  }
 }
