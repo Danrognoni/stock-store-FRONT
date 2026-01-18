b1db6b5e48cdeff58921a3ec4fa3262f7846236d
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-01-18 12:36:24 -0300

    Add address models

---
+++ src/app/models/address/address-det.ts
@@ -0,0 +1,10 @@
+export interface AddressDet {
+    id:string;
+    name:string;
+    street:string;
+    number:string;
+    apartment?:string;
+    zipCode:string;
+    city:string;
+    province:string;
+}
---
+++ src/app/models/address/address-request.ts
@@ -0,0 +1,9 @@
+export interface AddressRequest {
+    name:string;
+    street:string;
+    number:string;
+    apartment?:string;
+    zipCode:string;
+    city:string;
+    province:string;
+}
