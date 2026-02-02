89cee50f242194daf43f5a6dc68faf6f7aaa00a2
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-02-02 18:39:20 -0300

    Add app initializr so when you f5 the session keeps logged in

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
-
 import { routes } from './app.routes';
 import { authenticationInterceptor } from './services/authentication-interceptor';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { catchError, of, tap } from 'rxjs';
+import { AuthenticationService } from './services/authentication-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +12,17 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authenticationInterceptor])),
     provideZonelessChangeDetection(),
-    
-    
+    provideAppInitializer(() => {
+        const authService = inject(AuthenticationService);
+        
+        return authService.getProfile().pipe(
+          tap(user => {
+            console.log('Sesión restaurada:', user); 
+          }),
+          catchError(() => {
+            return of(null);
+          })
+        );
+    })
   ]
 };
