59df0a0dd49078a76a1a6fc415f3b00953fe2c50
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-02-04 23:19:10 -0300

    Update product-detail.ts

--- src/app/pages/product/product-detail/product-detail.ts
+++ src/app/pages/product/product-detail/product-detail.ts
@@ -1,10 +1,10 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { CommonModule } from '@angular/common'; // <--- IMPORTANTE para pipes y directivas
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; // <--- Agrega esto para el spinner
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; 
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ProductService } from '../../../services/product';
 import { ProductDet } from '../../../models/product/product-det';
@@ -12,7 +12,6 @@ import { ProductDet } from '../../../models/product/product-det';
 @Component({
   selector: 'app-product-detail',
   standalone: true, 
-  // Agrega CommonModule y MatProgressSpinnerModule a los imports
   imports: [CommonModule, MatCardModule, MatListModule, MatIconModule, MatButtonModule, RouterLink, MatProgressSpinnerModule],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
@@ -23,35 +22,34 @@ export class ProductDetail implements OnInit {
   public productService = inject(ProductService);
   
   public product = signal<ProductDet | null>(null);
-  public loading = signal<boolean>(true); // <--- Nueva señal para saber si está cargando
+  public loading = signal<boolean>(true);
 
   ngOnInit(): void {
-    // Usamos subscribe a paramMap para reaccionar si el ID cambia
     this.ruta.paramMap.subscribe(params => {
       const id = params.get('id');
       if (id) {
         this.getProductById(id);
       } else {
-        this.loading.set(false); // No hay ID, dejamos de cargar (mostrará error)
+        this.loading.set(false); 
       }
     });
   }
 
   getProductById(id: string) {
-    this.loading.set(true); // Empieza a cargar
+    this.loading.set(true);
     this.productService.getProduct(id).subscribe({
       next: (data) => {
         this.product.set(data);
-        this.loading.set(false); // Terminó de cargar
+        this.loading.set(false); 
       },
       error: (e) => {
         console.error('Error cargando producto:', e);
-        this.loading.set(false); // Terminó (con error)
+        this.loading.set(false); 
       }
     });
   }
 
   getBack() {
-    this.router.navigate(['/online-store']); // Volver al catálogo
+    this.router.navigate(['/online-store']); 
   }
 }
